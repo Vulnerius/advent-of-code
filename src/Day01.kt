fd62d002ee12d5982966b500514d09f955d406d4
Paul Fuchs <64081175+Vulnerius@users.noreply.github.com>
2025-12-01 22:09:14 +0100

    :test_tube: checking in right solution together with my first approach

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,93 @@
+import kotlin.math.abs
+
 fun main() {
+
+    // start at dial 50
+    // given ("L|RanyNumber"), L for decreasing the dial and R for increasing the dial
+    // if the dial goes below 0, it wraps around to 99
+    // if the dial goes above 99, it wraps around to 0
+    // count all times the dial lands on zero and return that count
     fun part1(input: List<String>): Int {
-        return input.size
+        var dial = 50
+        var password = 0
+        for (instruction in input) {
+            val direction = instruction[0].toString()
+            val amountStr = instruction.drop(1).trim().toInt()
+            when (direction) {
+                "L" -> dial = (dial - amountStr).mod(100)
+                "R" -> dial = (dial + amountStr).mod(100)
+            }
+            if (dial == 0)
+                password++
+        }
+        return password
     }
+
+    // start at dial 50
+    // given ("L|RAnyNumber"), L for decreasing the dial and R for increasing the dial
+    // if the dial goes below 0, it wraps around to 99
+    // if the dial goes above 99, it wraps around to 0
+    // count all times the dial passes zero (not only lands on it) and return that count
+    fun part2helped(input: List<String>): Int {
+        var dial = 50
+        var password = 0
+
+        for (instruction in input) {
+            val direction = if (instruction[0] == 'L') -1 else 1
+
+            repeat(instruction.drop(1).trim().toInt()) {
+                dial = (dial + direction).mod(100)
+                if (dial == 0)
+                    password++
+            }
+        }
+        return password
+    }
+
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var dial = 50
+        var password = 0
+
+        for (instruction in input) {
+            val direction = instruction[0]
+            val amountStr = instruction.drop(1).trim().toInt()
+            if (direction == 'L') {
+                dial = dial - amountStr
+                // dial can be smaller than -100, meaning we are passing 0 multiple times
+                if (dial < 0 && dial > -100)
+                    password++
+                else
+                    dial = abs(dial / 100)
+            } else {
+                dial = dial + amountStr
+                // dial can be larger than 100, meaning we are passing 0 multiple times
+                if (dial > 100 && dial < 200)
+                    password++
+                else
+                    password += (dial / 100)
+            }
+            dial = dial.mod(100)
+            if (dial == 0)
+                password++
+        }
+
+        return password
     }
 
+
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
+    println("########\n part 1:")
     part1(input).println()
+    println("########\n part 2:")
     part2(input).println()
+    println("########\n part 2_2:")
+    part2helped(input).println()
 }
