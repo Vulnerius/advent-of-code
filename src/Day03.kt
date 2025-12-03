725a687cd191b1e9df0f49fdba1117cea10f373c
Paul Fuchs <64081175+Vulnerius@users.noreply.github.com>
2025-12-03 08:23:06 +0100

    D03_p2 :white_check_mark:

--- src/Day03.kt
+++ src/Day03.kt
@@ -8,27 +8,41 @@ fun main() {
         val maxValues = input.map maxVals@ { line ->
             val digits = line.map { it.digitToInt() }
             val maxTen = digits.dropLast(1).max()
-            val maxTenIdx = (digits.dropLast(1).indexOfFirst { it == maxTen }) + 1
+            val maxTenIdx = (digits.indexOfFirst { it == maxTen }) + 1
             val maxOne = digits.drop(maxTenIdx).max()
-            println("$maxTen$maxOne for line: $line")
             return@maxVals maxTen * 10 + maxOne
         }
         return maxValues.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    // given multiple lines of strings, calculate the max 12-digit value per line
+    // by keeping the order and using string concatenation of the twelve highest digits
+    // return the sum of all max 12-digit values
+    fun part2(input: List<String>): Long {
+        val maxValues = input.map maxVals@ { line ->
+            val digits = line.map { it.digitToInt() }
+            var twelveBatteries = ""
+            var currentIdx = 0
+            for (i in 0..11) {
+                val currentMax = digits.drop(currentIdx).dropLast(11-i).max()
+                currentIdx = digits.drop(currentIdx).indexOfFirst { it == currentMax } + currentIdx +1
+                twelveBatteries = twelveBatteries.plus(currentMax)
+            }
+
+            return@maxVals twelveBatteries.toLong()
+        }
+
+        return maxValues.sum().toLong()
     }
 
 // test input per description from `src/Day03_test.txt` file:
     val testInput = readInput("Day03_test")
-    println(part1(testInput))
     check(part1(testInput) == 357)
-    //check(part2(testInput) == 1)
+    check(part2(testInput) == 3121910778619L)
 
 // aoc-personal-data from `input/Day03.txt` file:
     val input = readInput("Day03")
-    //check(part1(input) == 44487518055)
+    check(part1(input) == 17229)
     //check(part2(input) == 53481866137)
 
 
