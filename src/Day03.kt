7a210cdabd37cad6840d3ac902cd0e2927ce6fd5
Paul Fuchs <64081175+Vulnerius@users.noreply.github.com>
2025-12-03 07:49:50 +0100

    D03 :white_check_mark:

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,7 +1,21 @@
+import kotlin.math.max
+
 fun main() {
 
+
+    // given multiple lines of strings, calculate the max 2-digit value per line
+    // by keeping the order and using string concatenation of the two highest digits
+    // return the sum of all max 2-digit values
     fun part1(input: List<String>): Int {
-        return input.size
+        val maxValues = input.map maxVals@ { line ->
+            val digits = line.map { it.digitToInt() }
+            val maxTen = digits.dropLast(1).max()
+            val maxTenIdx = (digits.dropLast(1).indexOfFirst { it == maxTen }) + 1
+            val maxOne = digits.drop(maxTenIdx).max()
+            println("$maxTen$maxOne for line: $line")
+            return@maxVals maxTen * 10 + maxOne
+        }
+        return maxValues.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -10,6 +24,7 @@ fun main() {
 
 // test input per description from `src/Day03_test.txt` file:
     val testInput = readInput("Day03_test")
+    println(part1(testInput))
     check(part1(testInput) == 357)
     //check(part2(testInput) == 1)
 
@@ -17,6 +32,7 @@ fun main() {
     val input = readInput("Day03")
     //check(part1(input) == 44487518055)
     //check(part2(input) == 53481866137)
+
 
     println("########\n part 1:")
     println("P1 start: ${java.time.LocalDateTime.now()}")
